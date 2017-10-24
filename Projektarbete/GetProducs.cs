00ea6e2c52b130a1fc104504601f587345940466
wilari932 <Halo_55_wilmar@hotmail.com>
2017-10-24 10:35:18 +0200

    Initial commit

---
+++ Projektarbete/GetProducs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Projektarbete
+{
+   
+    class GetProducs 
+    {
+        private string path = @"Resources\products.txt";
+        public List<Product> Products = new List<Product>();
+
+        public GetProducs()
+        {
+
+            try
+            {
+
+                List<string> ReadLines = File.ReadAllLines(path).ToList();
+
+
+                foreach (string lines in ReadLines)
+                {
+
+                    string[] entries = lines.Split(',');
+                    Product s = new Product();
+                    s.Name = entries[0];
+                    s.PictureName = entries[1];
+                    s.Price = Double.Parse(entries[2]);
+                    Products.Add(s);
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Text Filen Finns inte !");
+            }
+
+
+            }
+        }
+      
+    }
+
---
+++ Projektarbete/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Projektarbete
+{
+    class Product
+    {
+
+        public string Name { get; set;  }
+        public double Price { get; set; }
+        public string PictureName{ get; set; }
+    }
+}
