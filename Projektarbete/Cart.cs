232378632e823a5ac245cfe7a8c8ee8d90059f7e
agustin-martinez <agustin.martinez@hotmail.com>
2017-10-27 11:16:38 +0200

    Fijate como quedoo ahora. Me parece que estaa mucho mejor. Le puse un toque de color y le agreguee los botones y pude cuadrar todo. Abrazo man! decime que onda si gusta o no. :)

--- Projektarbete/Cart.cs
+++ Projektarbete/Cart.cs
@@ -8,9 +8,9 @@ using System.Drawing;
 
 namespace Projektarbete
 {
-     class Cart
+    class Cart
     {
-      public  TableLayoutPanel PanelWithProducs { get; set; }
+        public TableLayoutPanel PanelWithProducs { get; set; }
         public void ProducsInCart(GetProducs GetProducsFromList)
         {
             GetProducs G = GetProducsFromList;
@@ -21,12 +21,18 @@ namespace Projektarbete
                 {
                     Height = 100,
                     Width = 400,
-                    ColumnCount = 2,
-                    RowCount = 1,
-                    CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble,
+                    ColumnCount = 5,
+                    //RowCount = 1,
+                    //CellBorderStyle = TableLayoutPanelCellBorderStyle.None,
                     BackColor = Color.White,
                     Dock = DockStyle.Top,
                 };
+                PanelWithProducs.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+                PanelWithProducs.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+                PanelWithProducs.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+                PanelWithProducs.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30));
+                PanelWithProducs.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+
                 PictureBox picture = new PictureBox
                 {
                     Dock = DockStyle.Fill,
@@ -37,34 +43,49 @@ namespace Projektarbete
                 {
                     Text = ProducFinder.Name + "\n" + ProducFinder.Price,
                     Font = new Font("Arial", 11, FontStyle.Regular),
-                    TextAlign = ContentAlignment.TopCenter,
+                    TextAlign = ContentAlignment.MiddleCenter,
                     Dock = DockStyle.Fill,
                 };
                 Button buttonMore = new Button
                 {
-                    //Name = a.Id.ToString(),
-                    Dock = DockStyle.Right,
-                    Text = "+",
-                    Font = new Font("Arial", 14, FontStyle.Regular),
-                    FlatStyle = FlatStyle.Standard,
-                    ForeColor = Color.Black,
-                    BackColor = Color.White
-                };
-                Button buttonMinus = new Button
-                {
-                    //Name = a.Id.ToString(),
                     Dock = DockStyle.Left,
-                    Text = "-",
-                    Font = new Font("Arial", 14, FontStyle.Regular),
-                    FlatStyle = FlatStyle.Standard,
+                    Text = "+",
+                    Font = new Font("Arial", 10, FontStyle.Regular),
+                    FlatStyle = FlatStyle.System,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Anchor = (AnchorStyles.None | AnchorStyles.None),
                     ForeColor = Color.Black,
                     BackColor = Color.White
                 };
-                PanelWithProducs.Controls.Add(picture);  
-                PanelWithProducs.Controls.Add(buttonMore);
-                PanelWithProducs.Controls.Add(info);
-                PanelWithProducs.Controls.Add(buttonMinus);
-            }
+                Button buttonLess = new Button
+                {
+                    Dock = DockStyle.Right,
+                    Text = "-",
+                    Font = new Font("Arial", 10, FontStyle.Regular),
+                    FlatStyle = FlatStyle.System,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Anchor = (AnchorStyles.None | AnchorStyles.None),
+                    ForeColor = Color.Black,
+                    BackColor = Color.White
+                };
+                TextBox quantity = new TextBox
+                {
+                    //Dock = DockStyle.Fill,
+                    TextAlign = HorizontalAlignment.Center,
+                    Text = "1",
+                    Anchor = (AnchorStyles.None | AnchorStyles.None),
+                    Font = new Font("Arial", 11, FontStyle.Regular),
+                    ForeColor = Color.Black,
+                    BackColor = Color.White
+                };
+
+
+            PanelWithProducs.Controls.Add(picture);
+            PanelWithProducs.Controls.Add(info);
+            PanelWithProducs.Controls.Add(buttonLess);
+            PanelWithProducs.Controls.Add(quantity);
+            PanelWithProducs.Controls.Add(buttonMore);
         }
     }
+}
 }
