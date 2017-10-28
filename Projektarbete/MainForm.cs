9ac079cba1fc185c9465183f2a1f3448b0ae4745
wilari932 <33053518+wilari932@users.noreply.github.com>
2017-10-28 14:21:06 +0200

    He arreglado un poco

--- Projektarbete/MainForm.cs
+++ Projektarbete/MainForm.cs
@@ -21,9 +21,6 @@ namespace Projektarbete
         public TableLayoutPanel VisualCartPanel { get; set; }
         public List<Product> CartProductsList2 = new List<Product>();
         private TableLayoutPanel Header { get; set; }
-        private PictureBox Header2 { get; set; }
-        private PictureBox Header3 { get; set; }
-        private PictureBox Header4 { get; set; }
         private Button Close1 { get; set; }
         private TableLayoutPanel a { get; set; }
 
@@ -34,27 +31,33 @@ namespace Projektarbete
         {
             a = new TableLayoutPanel
             {
-                Width = 970,
-            Height = 535,
-            RowCount = 1,
-            Dock = DockStyle.Fill
-        };
-            MessageBox.Show("d00");
-           a.RowStyles.Add(new RowStyle(SizeType.Percent, 90));
+                RowCount = 2,
+                Dock = DockStyle.Fill,
+                BackColor = Color.Black,
+
+
+
+                ///  CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble        
+
+            };
+            
+           a.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+            a.RowStyles.Add(new RowStyle(SizeType.Percent, 95));
 
             Width = 1000;
             Height = 575;
 
             Close1 = new Button
             {
-                Width = 45,
-                Height =30,
+              Height = 30,
+              Width = 30,
                 Text = "X",
-                Font = new Font("Arial", 11, FontStyle.Regular),
-                TextAlign = ContentAlignment.MiddleCenter,
-               // Anchor = (AnchorStyles.None | AnchorStyles.Left),
+                Font = new Font("Arial", 10, FontStyle.Bold),
+               TextAlign = ContentAlignment.MiddleCenter,
+                // Anchor = (AnchorStyles.None | AnchorStyles.Left),
                 Dock = DockStyle.Fill,
-               BackColor = Color.Orange
+                BackColor = Color.Orange,
+                 FlatStyle =  FlatStyle.Flat,
                 
                 
                 
@@ -74,43 +77,18 @@ namespace Projektarbete
 
             Header = new TableLayoutPanel
             {
-                Height = 50,
+                
 
-                Dock = DockStyle.Top,
-                BackColor = Color.Black,
+                Dock = DockStyle.Fill,
+                
                 ColumnCount = 2
                 
 
             };
             Header.Controls.Add(Close1,2,0);
-            Header.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 95));
-            Header2 = new PictureBox
-            {
-                Height = 5,
-                Dock = DockStyle.Bottom,
-                BackColor = Color.Black
-
-
-            };
-            Header3 = new PictureBox
-            {
-                Width = 5,
-                Dock = DockStyle.Right,
-                BackColor = Color.Black,
-              Cursor = Cursors.SizeWE
-                
-
-            };
-            Header4 = new PictureBox
-            {
-                Width = 5,
-                Dock = DockStyle.Left,
-                BackColor = Color.Black,
-                Cursor = Cursors.SizeWE
-
-
-
-            };
+            Header.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 90));
+           
+       
             LeftMenuPanel = new TableLayoutPanel { Dock = DockStyle.Fill, RowCount = 2 , };
             VisualCartPanel = new TableLayoutPanel { Dock = DockStyle.Fill, AutoScroll = true };
             LeftMenuPanelUp = new TableLayoutPanel { Dock = DockStyle.Fill, RowCount = 1, BackColor = Color.White, BorderStyle = BorderStyle.Fixed3D };
@@ -189,7 +167,9 @@ namespace Projektarbete
                     RowCount = 4,
                     Width = 175,
                     Height = 250,
-                    //CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetPartial        
+                 
+                    
+                    
                 };
                 productRangePanel.Controls.Add(boc);
                 productRangePanel.Controls.Add(labelName);
@@ -213,12 +193,10 @@ namespace Projektarbete
                 Dock = DockStyle.Fill
             };
            
-            this.Controls.Add(Header);
-            this.Controls.Add(Header2);
-            this.Controls.Add(Header3);
-            this.Controls.Add(Header4);
+          
             this.Controls.Add(a);
-            a.Controls.Add(RootPanel);
+            a.Controls.Add(Header, 0, 0);
+            a.Controls.Add(RootPanel,0,1);
             RootPanel.Controls.Add(RightMenuPanel,1,0);
            
 
